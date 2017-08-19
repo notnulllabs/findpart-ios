538b317f94be6ee55dd054e0ae18a547ae51b739
DinoPeople <dp@DinoPeopleui-MacBook-Pro.local>
2017-08-19 16:15:19 +0900

    Initial Commit

---
+++ FindPartyTests/FindPartyTests.swift
@@ -0,0 +1,36 @@
+//
+//  FindPartyTests.swift
+//  FindPartyTests
+//
+//  Created by DinoPeople on 2017. 8. 19..
+//  Copyright © 2017년 NotNullLabs. All rights reserved.
+//
+
+import XCTest
+@testable import FindParty
+
+class FindPartyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
